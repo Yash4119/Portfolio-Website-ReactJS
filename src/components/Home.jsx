009fe6fddadb6a77704e5496a3f457930b7e22b6
yash ambekar <yashamb444@gmail.com>
2022-10-28 21:34:25 +0530

    Production Build Push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row text-white">
         <div className="flex flex-col justify-center h-full mt-20">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Full Stack Developer
+            I'm a Problem Solver
           </h2>
           <p className="text-gray-400 py-4 px-1 max-w-md ">
             I'm a 3rd year Computer Engineering student at Dr. D. Y. Patil
